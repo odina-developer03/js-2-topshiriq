5b4cb36370b1d36e9c4a7058b52524388aaff5c9
Пользователь <odinaabdurahimova03@gmail.com>
2024-01-10 18:35:06 +0500

    Added files

---
+++ main.js
@@ -0,0 +1,20 @@
+let salom =alert("HUSH KELIBSIZ!!!")
+let sum =prompt ("1 dan 12 gacha bo`lgan sonlarni kiriting!")
+if(sum>=1 && sum<=2){
+    console.log("Qish❄")
+}
+else if(sum>=3 && sum<=5){
+    console.log("Bahor☘")
+}
+else if(sum>=6 && sum<=8){
+    console.log("Yoz🍀")
+}
+else if(sum>=9 && sum<=11){
+    console.log("kuz🍂")
+}
+else if(sum==12){
+    console.log("Qish❄")
+}
+else{
+    console.log("Kechirasiz, noto`g`ri raqam kiritdingiz!!!")
+}
